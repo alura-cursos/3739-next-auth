24d41f195690502f2e062929c1e011b18ccc46a6
Vinicios Neves <vinicios.neves@mac>
2024-04-13 17:19:08 +0100

    desafio resolvido da aula 2

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,16 +1,12 @@
 import Image from "next/image"
 import styles from './comment.module.css'
+import { Avatar } from "../Avatar"
 
 export const Comment = ({ comment }) => {
 
-    const imgSrc = comment.author.avatar ?? comment.author.image
 
     return (<div className={styles.comment}>
-        {imgSrc && <Image 
-            src={imgSrc} 
-            width={32} height={32} 
-            alt={`Avatar do(a) ${comment.author.name}`} 
-        />}
+        <Avatar author={comment.author} />
         <strong>@{comment.author.name}</strong>
         <p>{comment.text}</p>
     </div>)
