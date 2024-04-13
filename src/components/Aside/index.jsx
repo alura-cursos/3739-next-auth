7e65fde659075c2f554eaf1bc6fa107a1ecb39d0
Vinicios Neves <vinicios.neves@mac>
2024-04-13 16:23:16 +0100

    video 1.4

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -10,8 +10,13 @@ import { Login } from '../icons/Login'
 import { Button } from '../Button'
 
 import styles from './aside.module.css'
+import { getServerSession } from 'next-auth'
+import { options } from '@/app/api/auth/[...nextauth]/options'
 
-export const Aside = () => {
+export const Aside = async () => {
+
+    const session = await getServerSession(options)
+
     return (<aside className={styles.aside}>
         <nav>
             <ul>
@@ -43,12 +48,18 @@ export const Aside = () => {
                         Sobre nós
                     </AsideLink>
                 </li>
-                <li>
-                    <AsideLink href="/login?">
+                {!session && <li>
+                    <AsideLink href="/api/auth/signin">
                         <Login />
                         Login
                     </AsideLink>
-                </li>
+                </li>}
+                {session && <li>
+                    <AsideLink href="/api/auth/signout">
+                        <Login />
+                        Logout
+                    </AsideLink>
+                </li>}
             </ul>
         </nav>
     </aside>)
