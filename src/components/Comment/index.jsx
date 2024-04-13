612de94ca78dc42b07f3a0bb3a3f75c955c597ed
Vinicios Neves <vinicios.neves@mac>
2024-04-13 17:08:43 +0100

    video 2.3

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -2,12 +2,15 @@ import Image from "next/image"
 import styles from './comment.module.css'
 
 export const Comment = ({ comment }) => {
+
+    const imgSrc = comment.author.avatar ?? comment.author.image
+
     return (<div className={styles.comment}>
-        <Image 
-            src={comment.author.avatar} 
+        {imgSrc && <Image 
+            src={imgSrc} 
             width={32} height={32} 
             alt={`Avatar do(a) ${comment.author.name}`} 
-        />
+        />}
         <strong>@{comment.author.name}</strong>
         <p>{comment.text}</p>
     </div>)
