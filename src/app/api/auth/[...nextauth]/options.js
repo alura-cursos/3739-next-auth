4f64a365a9e813f90d55df93edc089674f10e368
Vinicios Neves <vinicios.neves@mac>
2024-04-15 11:12:33 +0100

    video 3.4

--- src/app/api/auth/[...nextauth]/options.js
+++ src/app/api/auth/[...nextauth]/options.js
@@ -3,8 +3,14 @@ import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 import db from "../../../../../prisma/db";
 
+import bcrypt from 'bcrypt'
+
 export const options = {
     adapter: PrismaAdapter(db),
+    session: {
+      strategy: 'jwt',
+      maxAge: 3000  
+    },
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
@@ -22,13 +28,45 @@ export const options = {
                     type: 'password',
                     placeholder: 'Digite sua senha!'
                 }
+            },
+            async authorize (credentials) { 
+
+                try {
+                    
+                    const foundUser = await db.user.findFirst({
+                        where: {
+                            email: credentials.email
+                        }
+                    })
+
+                    if (foundUser) {
+                        console.log('User encontrado')
+
+                        const passMatch = bcrypt.compareSync(
+                            credentials.password, 
+                            foundUser.password
+                        )
+
+                        if (passMatch) {
+                            console.log('usuario correto')
+                            delete foundUser.password
+                            return foundUser
+                        }
+                        
+                    }
+
+                } catch (error) {
+                    console.log('Erro ao autorizar um usuario', error)
+                }
+
+                return null;
             }
         })
     ],
     callbacks: {
-        async session({ session, user }) {
+        async session({ session, token }) {
             if (session?.user) {
-                session.user.id = user.id
+                session.user.id = parseInt(token.sub)
             }
             return session
         }
